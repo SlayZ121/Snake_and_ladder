74b0f4fc10ed5eae1a4b628623bb704f1922edc3
SlayZ121 <slayz9168@gmail.com>
2024-04-07 18:32:08 +0530

    commit

--- src/App.java
+++ src/App.java
@@ -3,32 +3,16 @@ import java.util.*;
 public class App {
     public static void main(String[] args) throws Exception {
         
-        Player p1=setPlayerDetailsFromUserInput();
-        
-        p1.getPlayerDetails();
+        Player p1=new Player();
+        p1=p1.setPlayerDetailsFromUserInput();
 
-        Player p2=setPlayerDetailsFromUserInput();
-        System.out.println(p2.getPlayerName());
+        Player p2=new Player();
+
+        p2=p2.setPlayerDetailsFromUserInput();
+    
+
+        System.out.println("Player name is: "+p1.getPlayerName());
+        System.out.println("Player name is: "+p2.getPlayerName());
     }
     
-    public static Player setPlayerDetailsFromUserInput(){
-        Scanner sc=new Scanner(System.in);
-        System.out.println("Enter details of the Player");
-        System.out.println("Enter the name of the player");
-        String name=sc.nextLine();
-        System.out.println("Enter Player's contact number");
-        String contactno=sc.nextLine();
-        System.out.println("Enter Player's address");
-        String address=sc.nextLine();
-        System.out.println("Enter Player's Email iD");
-        String email_id=sc.nextLine();
-        System.out.println("Enter Player's age");
-        int age=sc.nextInt();
-
-        Player p=new Player();
-        p.set_player_details(name,contactno, address, email_id, age);
-
-        return p;
-
-    }
 }
--- src/player/Player.java
+++ src/player/Player.java
@@ -1,5 +1,7 @@
 package player;
 
+import java.util.Scanner;
+
 public class Player {
       private String name;
       private String email_id;
@@ -29,4 +31,25 @@ public class Player {
      public String getPlayerName(){
         return this.name;
      }
+
+      public Player setPlayerDetailsFromUserInput(){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter details of the Player");
+        System.out.println("Enter the name of the player");
+        String name=sc.nextLine();
+        System.out.println("Enter Player's contact number");
+        String contactno=sc.nextLine();
+        System.out.println("Enter Player's address");
+        String address=sc.nextLine();
+        System.out.println("Enter Player's Email iD");
+        String email_id=sc.nextLine();
+        System.out.println("Enter Player's age");
+        int age=sc.nextInt();
+
+        Player p=new Player();
+        p.set_player_details(name,contactno, address, email_id, age);
+
+        return p;
+
+    }
 }
